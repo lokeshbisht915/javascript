9498970b2e51b53c4e218df264809bc79d4496a2
lokeshbisht915 <158795443+lokeshbisht915@users.noreply.github.com>
2025-07-02 10:51:27 +0000

    Dynamic nature of const

---
+++ object4_dynamicNature.js
@@ -0,0 +1,13 @@
+
+const circle = {
+    radius:1,
+    location:{
+        x:1,
+        y:1
+    }
+};
+//we can add some property so
+circle.color = "Blue";
+circle.draw = function(){
+    console.log('it creates a function');
+}
