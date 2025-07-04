5cd06dfa6aab2623e0ff836dac129e65449ddc90
lokeshbisht915 <158795443+lokeshbisht915@users.noreply.github.com>
2025-07-04 09:44:19 +0000

    Few methods of object explained

---
+++ obj8EnumeratingProperites.js
@@ -0,0 +1,18 @@
+const circle = {
+
+	Radius: 1,
+	Draw() {
+	    console.log('draw');
+	}
+};
+
+for(let key in circle)
+	console.log(key,circle[key]);
+
+for(let key of Object.key(circle))
+	console.log(key);
+
+for(let entry of Object.entries(circle))
+	console.log(entry);
+
+for('radius' in circle) console.log('yes');
